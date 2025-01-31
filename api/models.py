f6cc6960845f5c0ae12620bf2973d89ccca92737
YabTek <yeabsira.tekuamwork@a2sv.org>
2025-01-31 15:27:23 +0300

    (feat): add models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# User model
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
+
+# task model
+class Task(models.Model):
+    STATUS_CHOICES = [
+    ("todo", "To Do"),
+    ("in_progress", "In Progress"),
+    ("completed", "Completed"),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="todo")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
+   
+    def __str__(self):
+        return self.title
+
+# Checklist model
+class Checklist(models.Model):
+    title = models.CharField(max_length=255)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="checklists")
+
+    def __str__(self):
+        return self.title
+
